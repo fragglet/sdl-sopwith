478db3f69d5cfc27179a50b495f1681d9dbebe7b
Simon Howard <fraggle@soulsphere.org>
2023-05-16 18:21:37 -0400

    We can skip the part about writing to the FSF.

--- src/swmove.h
+++ src/swmove.h
@@ -7,11 +7,7 @@
 // Free Software Foundation; either version 2 of the License, or (at your
 // option) any later version. This program is distributed in the hope that
 // it will be useful, but WITHOUT ANY WARRANTY; without even the implied
-// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-// the GNU General Public License for more details. You should have
-// received a copy of the GNU General Public License along with this
-// program; if not, write to the Free Software Foundation, Inc., 59 Temple
-// Place - Suite 330, Boston, MA 02111-1307, USA.
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 //
 
 #ifndef __SWMOVE_H__
