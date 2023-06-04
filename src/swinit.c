fragglet/sdl-sopwith

commit 48dd75bc255a0819ebfff0c4dae426f0b2b5d36a

Eliminate nobjects[] pre-allocated array.
Objects were being allocated at the start of the level as a single contiguous
array of OBJECTS structures; this meant that we had a static limit of 100
objects that could be instantiated at once. There were also places where eg.
nobjects[0] was being used to refer to player 1's plane because
(coincidentally) that player would be the first object allocated when the level
is created.

This gets rid of the nobjects[] array entirely. For those cases whhere we used
it to number planes, add a new planes[] array. We also get rid of the ob_index
field, since this no longer has any meaning now that the objects are no longer
allocated contiguously.

src/swinit.c
@@ -16,6 +16,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <assert.h>
 
 #include "pcsound.h"
 #include "timer.h"
@@ -70,24 +71,13 @@ PACKAGE_STRING "\n"
 
 static void initobjs(void)
 {
-	OBJECTS *ob;
-	int i;
-
 	topobj.ob_xnext = topobj.ob_next = &botobj;
 	botobj.ob_xprev = botobj.ob_prev = &topobj;
 	topobj.ob_x = -32767;
 	botobj.ob_x = 32767;
 
 	objbot = objtop = deltop = delbot = NULL;
-	objfree = ob = nobjects;
-
-	for (i = 0; i < MAX_OBJS; ++i) {
-		ob->ob_next = ob + 1;
-		ob->ob_index = i;
-		++ob;
-	}
-
-	(ob-1)->ob_next = NULL;
+	objfree = NULL;
 }
 
 
@@ -232,16 +222,17 @@ static void get_endlevel(OBJECTS *ob)
 OBJECTS *initpln(OBJECTS * obp)
 {
 	OBJECTS *ob;
-	int x, height, minx, maxx, n;
+	int x, height, minx, maxx;
 
 	if (!obp) {
+		assert(num_planes < MAX_PLYR);
 		ob = allocobj();
+		ob->ob_original_ob = &currgame->gm_planes[num_planes];
+		planes[num_planes] = ob;
+		++num_planes;
 	} else {
 		ob = obp;
 	}
-
-	n = ob->ob_index;
-	ob->ob_original_ob = &currgame->gm_planes[n];
 
 	if (obp && ob->ob_state != CRASHED && !ob->ob_athome) {
 		/* Just returned home */
@@ -314,7 +305,7 @@ void initplyr(OBJECTS * obp)
 	if (!obp) {
 		ob->ob_drawf = dispplyr;
 		ob->ob_movef = moveplyr;
-		ob->ob_clr = ob->ob_index % 2 + 1;
+		ob->ob_clr = ob->ob_original_ob->owner % 2;
 		ob->ob_owner = ob;
 		endcount = 0;
 
@@ -341,7 +332,7 @@ void initcomp(OBJECTS * obp)
 		ob->ob_drawf = dispcomp;
 		ob->ob_movef = movecomp;
 		ob->ob_clr = 2;
-		ob->ob_owner = &nobjects[1];
+		ob->ob_owner = planes[1];
 	}
 	if (playmode == PLAYMODE_SINGLE || playmode == PLAYMODE_NOVICE) {
 		ob->ob_state = FINISHED;
@@ -627,22 +618,22 @@ static OBJECTS *inittarget(const original_ob_t *orig_ob)
 		case OWNER_PLAYER1:
 		case OWNER_PLAYER5:
 		case OWNER_PLAYER7:
-			ob->ob_owner = &nobjects[0];
+			ob->ob_owner = planes[0];
 			++numtarg[0];
 			break;
 		case OWNER_PLAYER2:
 		case OWNER_PLAYER4:
 		case OWNER_PLAYER6:
 		case OWNER_PLAYER8:
-			ob->ob_owner = &nobjects[1];
+			ob->ob_owner = planes[1];
 			++numtarg[1];
 			break;
 		case OWNER_PLAYER3:
 			if (playmode == PLAYMODE_ASYNCH) {
-				ob->ob_owner = &nobjects[0];
+				ob->ob_owner = planes[0];
 				++numtarg[0];
 			} else {
-				ob->ob_owner = &nobjects[1];
+				ob->ob_owner = planes[1];
 				++numtarg[1];
 			}
 			break;
@@ -925,6 +916,8 @@ void swinitlevel(void)
 	initobjs();
 
 	num_players = 0;
+	num_planes = 0;
+	memset(planes, 0, sizeof(planes));
 
 	if (keydelay == -1) {
 		keydelay = 1;
@@ -967,7 +960,7 @@ void swrestart(void)
 	int time;
 		
 	if (consoleplayer->ob_endsts == WINNER) {
-		ob = &nobjects[player];
+		ob = planes[player];
 		inc = 0;
 
 		get_endlevel(ob);

src/sw.h
@@ -248,7 +248,6 @@ typedef struct obj {                            /*  Object list             */
 	int            ob_ldx, ob_ldy;
 	struct obj    *ob_next;
 	struct obj    *ob_prev;
-	int            ob_index;
 	bool           ob_drwflg;
 	bool           ob_onmap;
 	void        ( *ob_drawf ) (struct obj *);

src/swcollsn.c
@@ -153,12 +153,12 @@ get_score_obj(OBJECTS *ob, int *reverse)
 	*reverse = 0;
 
 	if (playmode != PLAYMODE_ASYNCH) {
-		retval = &nobjects[0];
+		retval = planes[0];
 		if (ob->ob_clr == 1) {
 			*reverse = 1;
 		}
 	} else
-		retval = &nobjects[2 - ob->ob_clr];
+		retval = planes[2 - ob->ob_clr];
 
 	return retval;
 }
@@ -351,7 +351,7 @@ static void swkill(OBJECTS * ob1, OBJECTS * ob2)
 		}
 
 		if (state == FALLING) {
-			if (ob->ob_index == player) {
+			if (ob == consoleplayer) {
 				if (ttype == SHOT) {
 					swwindshot();
 				} else if (ttype == OX) {
@@ -365,7 +365,7 @@ static void swkill(OBJECTS * ob1, OBJECTS * ob2)
 
 		if (ttype == SHOT || ttype == BIRD
 		    || ttype == OX || ttype == FLOCK) {
-			if (ob->ob_index == player) {
+			if (ob == consoleplayer) {
 				if (ttype == SHOT) {
 					swwindshot();
 				} else if (ttype == OX) {

src/swmain.c
@@ -52,7 +52,9 @@ bool soundflg = 0;		/* Sound flag                       */
 
 int displx;			/* Display left and right           */
 
-OBJECTS *nobjects;		/* Objects list.                    */
+OBJECTS *planes[MAX_PLYR];      /* Plane objects                    */
+int num_planes;
+
 OBJECTS *objbot, *objtop,	/* Top and bottom of object list    */
 *objfree,			/* Free list                        */
 *deltop, *delbot;		/* Newly deallocated objects        */
@@ -192,8 +194,6 @@ static void dump_cmds(void)
 int swmain(int argc, char *argv[])
 {
 	int nexttic;
-
-	nobjects = (OBJECTS *) malloc(100 * sizeof(OBJECTS));
 
 	swinit(argc, argv);
 	setjmp(envrestart);

src/swmain.h
@@ -43,7 +43,8 @@ extern int     targrnge;
 extern bool    titleflg;
 extern bool    soundflg;
 extern int     displx;
-extern OBJECTS *nobjects;
+extern OBJECTS *planes[MAX_PLYR];
+extern int     num_planes;
 extern OBJECTS *objbot, *objtop, *objfree, *deltop, *delbot;
 extern OBJECTS topobj, botobj;
 extern OBJECTS *objsmax;

src/swobject.c
@@ -13,6 +13,8 @@
 //        swobject -      SW object allocation and deallocation
 //
 
+#include <assert.h>
+
 #include "sw.h"
 #include "swmain.h"
 #include "swobject.h"
@@ -21,12 +23,13 @@ OBJECTS *allocobj(void)
 {
 	OBJECTS *ob;
 
-	if (!objfree) {
-		return NULL;
+	if (objfree != NULL) {
+		ob = objfree;
+		objfree = ob->ob_next;
+	} else {
+		ob = calloc(1, sizeof(OBJECTS));
+		assert(ob != NULL);
 	}
-
-	ob = objfree;
-	objfree = ob->ob_next;
 
 	ob->ob_next = NULL;
 	ob->ob_prev = objbot;

commit ed4e98fd9b95693adb2dd28960e6a86a129890c2

Fix wrong color for player 2 in multiplayer.
This bug was mistakenly introduced in commit 48dd75bc255a0.

src/swinit.c
@@ -308,7 +308,7 @@ void initplyr(OBJECTS * obp)
 	if (!obp) {
 		ob->ob_drawf = dispplyr;
 		ob->ob_movef = moveplyr;
-		ob->ob_clr = ob->ob_original_ob->owner % 2;
+		ob->ob_clr = ob->ob_original_ob->owner;
 		ob->ob_owner = ob;
 		endcount = 0;
 