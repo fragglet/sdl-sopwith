8273d5b1d5f4a147cffe4b5650177a39f34de65a
Derek Quenneville <techknight@techknight.com>
2023-11-19 23:09:07 -0500

    Add quit to menu hotkey (ctrl+q). Add quick restart hotkey (ctrl+r).

--- src/sdl/video.c
+++ src/sdl/video.c
@@ -43,6 +43,8 @@ bool vid_fullscreen = false;
 
 extern unsigned char *vid_vram;
 extern unsigned int vid_pitch;
+extern void swinitlevel(void);
+extern void swrestart(void);
 
 int keybindings[NUM_KEYS] = {
 	0,                    // KEY_UNKNOWN
@@ -573,6 +575,10 @@ static void getevents(void)
 						"user aborted with 3 ^C's\n");
 					exit(-1);
 				}
+			} else if (ctrldown && event.key.keysym.sym == SDLK_r) {
+					swinitlevel();
+			} else if (ctrldown && event.key.keysym.sym == SDLK_q) {
+					swrestart();
 			} else if (event.key.keysym.sym == SDLK_RETURN) {
 				if (altdown) {
 					vid_fullscreen = !vid_fullscreen;
