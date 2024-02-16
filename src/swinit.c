fragglet/sdl-sopwith

commit aaec2872c62800e3d23f32fdf95cfeda5fcd9ae9

Improve multiplayer

src/swinit.c
@@ -141,19 +141,6 @@ static void initseed()
 // status bar code
 //
 
-static void initscore()
-{
-	if (savescore) {
-		nobjects[0].ob_score = savescore;
-		savescore = 0;
-	}
-
-	dispscore(&nobjects[0]);
-	if (playmode == PLAYMODE_ASYNCH)
-		dispscore(&nobjects[1]);
-}
-
-
 
 
 static void dispgge(int x, int cury, int maxy, int clr)
@@ -288,7 +275,6 @@ void initdisp(BOOL reset)
 		swtitlf();
 		ghost = FALSE;
 	}
-	initscore();
 
 	ob = &nobjects[player];
 	if (ghost) {
@@ -298,8 +284,6 @@ void initdisp(BOOL reset)
 		ghostob.ob_newsym = symbol_ghost;     // sdh 27/6/2002
 		swputsym(GHOSTX, 12, &ghostob);
 	}
-
-	dispinit = TRUE;
 }
 
 
@@ -398,7 +382,6 @@ OBJECTS *initpln(OBJECTS * obp)
 void initplyr(OBJECTS * obp)
 {
 	OBJECTS *ob;
-	OBJECTS *initpln();
 
 	ob = initpln(obp);
 	if (!obp) {
@@ -409,7 +392,15 @@ void initplyr(OBJECTS * obp)
 		oobjects[ob->ob_index] = *ob;  // sdh 16/11: removed movmem
 		goingsun = FALSE;
 		endcount = 0;
-		consoleplayer = ob;
+
+		ob->ob_plrnum = num_players;
+		++num_players;
+
+		/* todo: save pointers to all player planes
+		 * and turn consoleplayer into a macro */
+
+		if (ob->ob_plrnum == player)
+			consoleplayer = ob;
 	}
 }
 
@@ -956,13 +947,10 @@ static void initgdep()
 
 void swinitlevel()
 {
+	int i;
+
 	if (playmode == PLAYMODE_ASYNCH)
 		init1asy();
-
-	// sdh 16/11/2001: this needs to be reset with each new game
-	// to keep netgames in sync if we have already played
-
-	countmove = 0;
 
 	// sdh: dont carry hud splats forward from previous games
 
@@ -971,6 +959,8 @@ void swinitlevel()
 	initsndt();
 	initgrnd();
 	initobjs();
+
+	num_players = 0;
 
 	if (keydelay == -1)
 		keydelay = 1;
@@ -1001,6 +991,14 @@ void swinitlevel()
 	initgdep();
 
 	inplay = TRUE;
+
+	// sdh 16/11/2001: this needs to be reset with each new game
+	// to keep netgames in sync if we have already played
+
+	countmove = 0;
+
+	for (i=0; i<num_players; ++i)
+		latest_player_time[i] = 0;
 }
 
 void swrestart()
@@ -1126,7 +1124,6 @@ void swinit(int argc, char *argv[])
 	if (modeset && keyset)
 		titleflg = TRUE;
 
-	movemax = 15;
 	initseed();
 
 	// sdh 27/6/2002: generate symbol objects
@@ -1164,6 +1161,9 @@ void swinit(int argc, char *argv[])
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.15  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.14  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

src/sw.h
@@ -261,6 +261,7 @@ typedef struct obj {                            /*  Object list             */
         int            ob_bursts;
         int            ob_bfiring;
         int            ob_bsdelay;
+	int            ob_plrnum;
 }       OBJECTS;
 
 typedef struct {                                /*  Game structure          */
@@ -290,6 +291,9 @@ static inline int SIN(int x) {
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.8  2004/10/15 21:30:57  fraggle
+// Improve multiplayer
+//
 // Revision 1.7  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

src/swasynio.c
@@ -46,11 +46,13 @@ char asynhost[128];
 
 static int lastkey = 0;		/*  Always behind one character     */
 
+#define TIMEOUT_LEN_MS 5000 	/* time out after 5 seconds */
+
 static int timeout_time;
 
-static void settimeout(int ms)
+static void settimeout()
 {
-	timeout_time = Timer_GetMS() + ms;
+	timeout_time = Timer_GetMS() + TIMEOUT_LEN_MS;
 }
 
 static BOOL timeout()
@@ -64,16 +66,16 @@ static inline void sendshort(int s)
 	commout((s >> 8) & 0xff);
 }
 
-static inline int readshort()
+static inline int try_readshort()
 {
 	int s, t;
 
-	while ((s = commin()) < 0) {
-		if (timeout()) {
-			fprintf(stderr, "readshort: timeout on read\n");
-			exit(-1);
-		}
-	}
+	s = commin();
+
+	if (s < 0)
+		return -1;
+
+	settimeout();
 
 	while ((t = commin()) < 0) {
 		if (timeout()) {
@@ -85,11 +87,27 @@ static inline int readshort()
 	return (t << 8) + s;
 }
 
+static int readshort()
+{
+	int i;
+
+	settimeout();
+
+	for (i=-1; i < 0; i = try_readshort()) {
+		if (timeout()) {
+			fprintf(stderr, "readshort: timeout on read\n");
+			exit(-1);
+		}
+	}
+
+	return i;
+}
+
 static int asynin()
 {
 	register int c;
 
-	settimeout(500);
+	settimeout();
 
         for (;;) {
 		if ((c = commin()) >= 0)
@@ -110,7 +128,6 @@ int asynget(OBJECTS * ob)
 		key = lastkey;
 		lastkey = 0;
 	} else {
-		settimeout(1000);
 		key = readshort();
 	}
 
@@ -119,12 +136,6 @@ int asynget(OBJECTS * ob)
 
 void asynput(int movekey)
 {
-	static BOOL first = TRUE;
-	
-	if (first)
-		first = FALSE;
-	else 
-
 	sendshort(movekey);
 }
 
@@ -134,6 +145,25 @@ char *asynclos(BOOL update)
 	return NULL;
 }
 
+
+void asynupdate(void)
+{
+	int i;
+
+	i = try_readshort();
+
+	if (i >= 0) {
+		int netplayer;
+
+		/* we have read a short from the other player. add
+		 * to the queue */
+
+		netplayer = !player;
+
+		latest_player_commands[netplayer][latest_player_time[netplayer] % MAX_NET_LAG] = i;
+		++latest_player_time[netplayer];
+	}
+}
 
 // this function is called by the multiplayer planes
 
@@ -216,12 +246,10 @@ static void synchronize()
 	free(buf);
 
 	if (player) {
-		settimeout(1000);
 		explseed = readshort();
 
 		printf("random seed: %i\n", explseed);
 
-		settimeout(1000);
 		conf_missiles = readshort() != 0;
 		conf_wounded = readshort() != 0;
 		conf_animals = readshort() != 0;
@@ -366,12 +394,16 @@ void init2asy()
 	register OBJECTS *ob;
 	OBJECTS *initpln();
 
+	initplyr(NULL);
+	initplyr(NULL);
+	/*
+
 	if (!player)
 		initplyr(NULL);
 
 	ob = initpln(NULL);
-	ob->ob_drawf = dispmult;
-	ob->ob_movef = movemult;
+	ob->ob_drawf = dispplyr;
+	ob->ob_movef = moveplyr;
 	ob->ob_clr = ob->ob_index % 2 + 1;
 	ob->ob_owner = ob;
 	ob->ob_state = FLYING;
@@ -381,11 +413,15 @@ void init2asy()
 
 	if (player)
 		initplyr(NULL);
+		*/
 }
 
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.7  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.6  2004/10/15 17:52:31  fraggle
 // Clean up compiler warnings. Rename swmisc.c -> swtext.c as this more
 // accurately describes what the file does.

src/swasynio.h
@@ -34,6 +34,7 @@ extern void asynput(int movekey);
 extern char *asynclos(BOOL update);
 extern void init1asy();
 extern void init2asy();
+extern void asynupdate(void);
 
 #endif
 
@@ -41,6 +42,9 @@ extern void init2asy();
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.4  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.3  2003/06/08 18:41:01  fraggle
 // Merge changes from 1.7.0 -> 1.7.1 into HEAD
 //

src/swcollsn.c
@@ -279,7 +279,8 @@ static void swkill(OBJECTS * ob1, OBJECTS * ob2)
 		initexpl(ob, 0);
 
 		scoretarg(ob, ob->ob_orient == 2 ? 200 : 100);
-		if (!--numtarg[ob->ob_clr - 1])
+		--numtarg[ob->ob_clr - 1];
+		if (numtarg[ob->ob_clr - 1] <= 0)
 			endgame(ob->ob_clr);
 		return;
 
@@ -491,6 +492,9 @@ void scorepln(OBJECTS * ob)
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.10  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.9  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

src/swdisp.c
@@ -112,37 +112,19 @@ void dispcomp(OBJECTS * ob)
 	plnsound(ob);
 }
 
-
-
-
-void dispmult(OBJECTS * ob)
-{
-	plnsound(ob);
-}
-
-
-
-
 void disptarg(OBJECTS * ob)
 {
 	if (ob->ob_firing)
 		sound(S_SHOT, 0, ob);
 }
 
-
-
-
 void dispflck(OBJECTS * ob)
 {
 }
 
-
-
-
 void dispbird(OBJECTS * ob)
 {
 }
-
 
 void dispplyr(OBJECTS * ob)
 {
@@ -161,6 +143,9 @@ void dispscore(OBJECTS * ob)
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.9  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.8  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

src/swdisp.h
@@ -30,7 +30,6 @@ extern void dispmiss(OBJECTS *obp);
 extern void dispburst(OBJECTS *obp);
 extern void dispexpl(OBJECTS *obp);
 extern void dispcomp(OBJECTS *ob);
-extern void dispmult(OBJECTS *ob);
 extern void disptarg(OBJECTS *ob);
 extern void dispflck(OBJECTS *ob);
 extern void dispbird(OBJECTS *ob);
@@ -43,6 +42,9 @@ extern void dispscore(OBJECTS *obp);
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.5  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.4  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

src/swgrpha.c
@@ -183,7 +183,6 @@ void swdisp()
 
 	dispgrnd();
 
-	dispinit = FALSE;
 	forcdisp = TRUE;
 
 	// need to update the screen as we arent writing
@@ -213,6 +212,9 @@ void colorscreen(int color)
 //---------------------------------------------------------------------------
 //
 // $Log$
+// Revision 1.11  2004/10/15 21:30:58  fraggle
+// Improve multiplayer
+//
 // Revision 1.10  2004/10/15 18:51:24  fraggle
 // Fix the map. Rename dispworld to dispmap as this is what it really does.
 //

commit ba78c26e76bd54635a5ba691353fa43048f9f83c

Fix score being reset between levels.
It seems that commit aaec2872c62800 broke the "savescore" variable that
saves the score when restarting the level. Fixes #20.

src/swinit.c
@@ -39,7 +39,7 @@
 static const original_ob_t *orig_planes[MAX_PLYR * 2];
 GRNDTYPE *ground;
 
-static int have_savescore = 0;
+static bool have_savescore = false;
 static score_t savescore;		/* save players score on restart  */
 int starting_level = 0;
 
@@ -113,6 +113,11 @@ void initdisp(bool reset)
 static void initscore(score_t *score)
 {
 	memset(score, 0, sizeof(score_t));
+
+	if (have_savescore) {
+		*score = savescore;
+		have_savescore = false;
+	}
 }
 
 static void initflightscore(flight_score_t *score)
@@ -999,12 +1004,12 @@ void swrestart(void)
 		}
 		++gamenum;
 		savescore = ob->ob_score;
-		have_savescore = 1;
+		have_savescore = true;
 	} else {
 		// gamenum = 0;
 		// allow variable start level -- Jesse
 		gamenum = starting_level;
-		have_savescore = 0;
+		have_savescore = false;
 
 		// sh 28/10/2001: go back to the title screen
 