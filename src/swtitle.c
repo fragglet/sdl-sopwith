97ccdd2e19333fe483943a715b0fa53b19b2d68f
Simon Howard <fraggle@soulsphere.org>
2024-02-16 20:20:58 -0500

    Fix plane direction.
    This got changed by mistake when doing the rotation/mirroring changes.

--- src/swtitle.c
+++ src/swtitle.c
@@ -87,7 +87,7 @@ void swtitln(void)
 
 	Vid_DispSymbol(40+X_OFFSET, 180, &symbol_plane[0]->sym[0],
 	               OWNER_PLAYER1);
-	Vid_DispSymbol(130+X_OFFSET, 80, &symbol_plane[3]->sym[5],
+	Vid_DispSymbol(130+X_OFFSET, 80, &symbol_plane[1]->sym[6],
 	               OWNER_PLAYER2);
 	Vid_DispSymbol(23+X_OFFSET, ground[530] + 16,
 	               &symbol_targets[3]->sym[0], OWNER_PLAYER2);
