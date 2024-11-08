d7611fddc55fd4ccded33021419227fe8290f957
Simon Howard <fraggle@soulsphere.org>
2024-11-07 19:01:49 -0500

    Add zoom link after image loads, if necessary

--- image-links.js
+++ image-links.js
@@ -1,13 +1,25 @@
 // Changes all image tags to clickable links if the source image is
 // significantly larger than the size on screen
-for (const img of document.getElementsByTagName("img")) {
-  var w = img.width;
-  if (w > 100 && img.naturalWidth > w * 1.5) {
-    var link = document.createElement("a");
-    link.href = img.src;
-    img.parentElement.insertBefore(link, img);
-    img.parentElement.removeChild(img);
-    img.style.cursor = "zoom-in";
-    link.appendChild(img);
+function afterImageLoaded(img, callback) {
+  if (img.complete) {
+    callback(img);
+  } else {
+    img.onload = function() {
+      callback(img);
+    }
   }
+}
+
+for (const img of document.getElementsByTagName("img")) {
+  afterImageLoaded(img, function(img) {
+    var w = img.width;
+    if (w > 100 && img.naturalWidth > w * 1.5) {
+      var link = document.createElement("a");
+      link.href = img.src;
+      img.parentElement.insertBefore(link, img);
+      img.parentElement.removeChild(img);
+      img.style.cursor = "zoom-in";
+      link.appendChild(img);
+    }
+  });
 }
